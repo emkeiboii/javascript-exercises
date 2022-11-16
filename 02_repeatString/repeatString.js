e8978fbba96ab057912948f571035d8f4d4c73c8
Mark <marklajom.work@gmail.com>
2022-11-16 14:52:07 +0100

    Finish exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  let stringHolder = "";
+  for(let i = 0; i < num; i++){
+    stringHolder += string;
+  } if (num < 0){
+    stringHolder = 'ERROR'
+  }
+  return stringHolder
 };
+
+const randomNumber = (Math.floor(Math.random)*1000)
+
+repeatString('hey', 3)
+repeatString('hey', 10)
+repeatString('hey', 1)
+repeatString('hey', 0)
+repeatString('hey', randomNumber)
+repeatString('', 10)
 
 // Do not edit below this line
 module.exports = repeatString;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  temp = (temp - 32) * 5/9; 
+  const rounded = Math.round(temp * 10) / 10;
+  return rounded
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  temp = (temp * 9/5) + 32;
+  const rounded = Math.round(temp * 10) / 10;
+  return rounded
 };
 
 // Do not edit below this line
