018a475d38fb2deb0a3f94c0b1824ac03f188573
Mark <marklajom.work@gmail.com>
2024-07-24 16:32:36 +0200

    finished ex08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,24 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((a, b) => a + b, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((a, b) => a * b);
 };
 
-const power = function() {
-	
+const power = function (base, index) {
+  return Math.pow(base, index);
 };
-
-const factorial = function() {
-	
-};
+//SKIPPED
+const factorial = function () {};
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +27,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
