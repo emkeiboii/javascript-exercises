e011fcfd04a02324b3174b5b99dd411b96a25210
Mark <marklajom.work@gmail.com>
2024-07-25 16:57:53 +0200

    finish ex09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let punctuationLess = str
+    .toLowerCase()
+    .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+    .replace(/\s{2,}/g, " ");
+  let clean = punctuationLess.concat("");
 
+  let cleanStr = clean
+    .split("")
+    .filter((spc) => spc !== " ")
+    .join("");
+  let reversedStr = clean
+    .split("")
+    .reverse()
+    .filter((spc) => spc !== " ")
+    .join("");
+
+  let isPalindrome = cleanStr === reversedStr;
+  return isPalindrome;
 };
 
 // Do not edit below this line
