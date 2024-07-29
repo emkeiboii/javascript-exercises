dd453406e45ed3fe00e58fd2842724141d91ff43
Mark <marklajom.work@gmail.com>
2024-07-29 07:57:51 +0200

    finish ex10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  let number = Number(num);
+  if (number < 0) return "OOPS";
 
+  let num0 = 0;
+  let num1 = 1;
+  let nextNum;
+  for (let i = 2; i <= number; i++) {
+    nextNum = num0 + num1;
+    num0 = num1;
+    num1 = nextNum;
+  }
+  return number === 0 ? num0 : num1;
 };
 
 // Do not edit below this line
