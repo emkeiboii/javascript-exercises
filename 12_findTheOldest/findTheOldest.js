67536b8d16437105c540b9af3699dfbbade8c779
Mark <marklajom.work@gmail.com>
2024-07-29 10:06:00 +0200

    finish ex12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const currYear = new Date().getFullYear();
 
+const findTheOldest = function (arr) {
+  const oldest = arr
+    .map((person) => ({
+      year: (person.yearOfDeath || currYear) - person.yearOfBirth,
+      name: person.name,
+    }))
+    .sort((a, b) => b.year - a.year)
+    .at(0);
+
+  return oldest;
 };
 
 // Do not edit below this line
